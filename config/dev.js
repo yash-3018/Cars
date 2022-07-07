9c6ce3b8fbf68bd328eda3e80fffaf1a542fa641
yash-3018 <yashagarwal3018@gmail.com>
2022-07-07 18:20:56 +0530

    first commit

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports={
+    MongoDB: 'mongodb+srv://harshi:24011976**@cluster0.tuwgx.mongodb.net/?retryWrites=true&w=majority',
+    FBAppID: '1369676290210234',
+    FBAppSECRET: '83c6fc5316b0b1418216dfc64efeb039',
+    AWSAccessID: 'AKIA53NX4CJVFO2MM7TL',
+    AWSSecretKey: '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'
+};
